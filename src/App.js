21f1b78ce0edfa1c2368c6b241cf0016dfa95d3c
TolikaTolika123 <tolikatolika794@gmail.com>
2022-05-13 14:38:17 +0400

    added homepage

--- src/App.js
+++ src/App.js
@@ -1,38 +1,29 @@
 import { useState, useEffect, useRef } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 import Header from "./components/Header/Header";
-import DropdownMenu from "./components/DropdownMenu/DropdownMenu";
+import Beach from './pages/Beach';
+import Home from './pages/Home';
 import './styles/App.css'
 import { convertSeconds } from './utils';
 
-import beachImg from './images/beach.jpg';
-import waldoImg from './images/waldo.png';
-import odlawImg from './images/odlaw.png';
-import wizardImg from './images/wizard.png';
-
-
 function App() {
   const [menuVisible, setMenuVisible] = useState(false)
-  const [popupStart, setPopupStart] = useState(true)
   const [timer, setTimer] = useState(0)
-  const [popupPlayAgain, setPopupPlayAgain] = useState(false)
   const [headerHeight, setHeaderHeight] = useState(0)
-  const [mousePos, setMousePos] = useState({ x: 0, y: 0 })
   const headerRef = useRef(null)
-  const [characters, setCharacters] = useState([]);
   const [timerID, setTimerID] = useState();
+  const [characters, setCharacters] = useState([]);
+
 
   useEffect(() => {
     if (headerRef) setHeaderHeight(headerRef.current.clientHeight)
   })
 
-  // checking after every move if user found all characters
-  useEffect(() => {
-    if (characters.length !== 0 && characters.every(character => character.found === true)) {
-      setPopupPlayAgain(true)
-      timerWork.endTimer();
-    }
-  }, [characters]);
+  const hideMenu = () => {
+    setMenuVisible(false)
+  }
 
   // object which allows to start timer and end it
   const timerWork = {
@@ -47,70 +38,29 @@ function App() {
     }
   }
 
-  // executes when user starts game first time
-  const startGame = () => {
-    setPopupStart(false);
-    setCharacters([
-      { name: 'waldo', found: false, img: waldoImg, id: 1 },
-      { name: 'odlaw', found: false, img: odlawImg, id: 2 },
-      { name: 'wizard', found: false, img: wizardImg, id: 3 }
-    ])
-    const startTime = Date.now();
-    timerWork.startTimer(startTime)
-  }
-
-  // executes when user starts game after winning
-  const startGameAgain = () => {
-    setPopupPlayAgain(false)
-    setCharacters([
-      { name: 'waldo', found: false, img: waldoImg, id: 1 },
-      { name: 'odlaw', found: false, img: odlawImg, id: 2 },
-      { name: 'wizard', found: false, img: wizardImg, id: 3 }
-    ])
-
-    const startTime = Date.now();
-    timerWork.startTimer(startTime)
-  }
-
-  // showing and hiding menu with characters on clicks
-  const showMenu = e => {
-    e.stopPropagation();
-    setMenuVisible(true)
-    setMousePos({ x: e.pageX, y: e.pageY - headerHeight })
-    console.log(mousePos)
-  }
-
-  const hideMenu = () => {
-    setMenuVisible(false)
-  }
-
   return (
     <div className="App" onClick={hideMenu}>
-      <Header timer={convertSeconds(timer)} ref={headerRef} characters={characters} />
-      {
-        popupStart &&
-        <div className="start__popup">
-          <div className="start__popup-content">
-            <p className='start__popup-text'>Where's Waldo</p>
-            <button onClick={startGame} className='start__popup-btn'>Start Game!</button>
-          </div>
-        </div>
-      }
-      {
-        popupPlayAgain &&
-        <div className="start__popup">
-          <div className="start__popup-content">
-            <p className='start__popup-text'>Congratulations, You won in {convertSeconds(timer)}! Wanna Play again?</p>
-            <button onClick={startGameAgain} className='start__popup-btn'>Play Again!</button>
-          </div>
-        </div>
-      }
-
-      <div className="game-field">
-        <img src={beachImg} alt="Game Field" onClick={showMenu} className='game-bg' />
-        {menuVisible && <DropdownMenu pos={mousePos} characters={characters} setCharacters={setCharacters} />}
-      </div>
+      <BrowserRouter >
+      <Header timer={convertSeconds(timer)} ref={headerRef} characters={characters} timerWork={timerWork} setTimer={setTimer} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/beach"
+            element={<Beach {...{
+              characters,
+              setTimer,
+              setMenuVisible,
+              headerHeight,
+              setCharacters,
+              menuVisible,
+              timer,
+              timerWork
+            }} />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
+
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,34 +1,56 @@
 import React from 'react'
 import cl from './Header.module.css'
+import { useLocation } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+
+import waldoImg from '../../images/waldo.png'
 
 import { firstLetterToUpperCase } from '../../utils'
 
-const Header = React.forwardRef(({ characters, timer }, ref) => {
-  return (
-    <header ref={ref} className={cl.header}>
-      <button className={cl.homeBtn}>Return to home</button>
-      <p className={cl.timer}>{timer}</p>
-      <div className={cl.findList}>
-        {characters.map(character => {
-          if (character.found === false) {
-            return (
-              <div key={character.id} className={cl.findItem}>
-                <img src={character.img} alt={character.name} />
-                <p>{firstLetterToUpperCase(character.name)}</p>
-              </div>
-            )
-          } else {
-            return (
-              <div key={character.id} className={`${cl.findItem} ${cl.overlay}`}>
-                <img src={character.img} alt={character.name} />
-                <p>{firstLetterToUpperCase(character.name)}</p>
-              </div>
-            )
-          }
-        })}
-      </div>
-    </header>
-  )
+const Header = React.forwardRef(({ characters, timer, timerWork, setTimer }, ref) => {
+  const {pathname} = useLocation();
+  
+  const stopTimer = () => {
+    timerWork.endTimer();
+    setTimer(0);
+  }
+  if (pathname !== '/') {
+    return (
+      <header ref={ref} className={cl.header}>
+        <Link onClick={stopTimer} to='/' className={cl.homeBtn}>Return to home</Link>
+        <p className={cl.timer}>{timer}</p>
+        <div className={cl.findList}>
+          {characters.map(character => {
+            if (character.found === false) {
+              return (
+                <div key={character.id} className={cl.findItem}>
+                  <img src={character.img} alt={character.name} />
+                  <p>{firstLetterToUpperCase(character.name)}</p>
+                </div>
+              )
+            } else {
+              return (
+                <div key={character.id} className={`${cl.findItem} ${cl.overlay}`}>
+                  <img src={character.img} alt={character.name} />
+                  <p>{firstLetterToUpperCase(character.name)}</p>
+                </div>
+              )
+            }
+          })}
+        </div>
+      </header>
+    )
+  } else {
+    return (
+      <header ref={ref} className={cl.header}>
+        <button className={cl.homeBtn}>Leaderboard</button>
+        <h1 className={cl.title}>Where's Waldo</h1>
+        <img className={cl.waldoImg} src={waldoImg} alt="Waldo" />
+      </header>
+    )
+  }
+
+  
 })
 
 export default Header
